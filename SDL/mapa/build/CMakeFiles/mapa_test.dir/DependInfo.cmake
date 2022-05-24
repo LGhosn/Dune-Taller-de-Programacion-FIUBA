388a373b917bcb686526182b1a0e1fd1a938baed
Joaquin Velazquez <jvelazquez@fi.uba.ar>
2022-05-24 13:43:51 -0300

    Test de mapa con musica

---
+++ SDL/mapa/build/CMakeFiles/mapa_test.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/mapa/sdl_mapa_main.cpp" "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/mapa/build/CMakeFiles/mapa_test.dir/sdl_mapa_main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "RESOURCE_PATH=\"/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/mapa/../resources\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/third_party/SDL2pp"
+  "SDL2pp/build"
+  "/usr/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/mapa/build/SDL2pp/build/CMakeFiles/SDL2pp.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ SDL/mapa/build/SDL2pp/build/CMakeFiles/SDL2pp.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/third_party/SDL2pp/SDL2pp/AudioDevice.cc" "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/mapa/build/SDL2pp/build/CMakeFiles/SDL2pp.dir/SDL2pp/AudioDevice.cc.o"
+  "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/third_party/SDL2pp/SDL2pp/AudioLock.cc" "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/mapa/build/SDL2pp/build/CMakeFiles/SDL2pp.dir/SDL2pp/AudioLock.cc.o"
+  "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/third_party/SDL2pp/SDL2pp/AudioSpec.cc" "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/mapa/build/SDL2pp/build/CMakeFiles/SDL2pp.dir/SDL2pp/AudioSpec.cc.o"
+  "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/third_party/SDL2pp/SDL2pp/Chunk.cc" "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/mapa/build/SDL2pp/build/CMakeFiles/SDL2pp.dir/SDL2pp/Chunk.cc.o"
+  "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/third_party/SDL2pp/SDL2pp/Color.cc" "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/mapa/build/SDL2pp/build/CMakeFiles/SDL2pp.dir/SDL2pp/Color.cc.o"
+  "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/third_party/SDL2pp/SDL2pp/Exception.cc" "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/mapa/build/SDL2pp/build/CMakeFiles/SDL2pp.dir/SDL2pp/Exception.cc.o"
+  "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/third_party/SDL2pp/SDL2pp/Font.cc" "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/mapa/build/SDL2pp/build/CMakeFiles/SDL2pp.dir/SDL2pp/Font.cc.o"
+  "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/third_party/SDL2pp/SDL2pp/Mixer.cc" "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/mapa/build/SDL2pp/build/CMakeFiles/SDL2pp.dir/SDL2pp/Mixer.cc.o"
+  "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/third_party/SDL2pp/SDL2pp/Music.cc" "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/mapa/build/SDL2pp/build/CMakeFiles/SDL2pp.dir/SDL2pp/Music.cc.o"
+  "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/third_party/SDL2pp/SDL2pp/Point.cc" "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/mapa/build/SDL2pp/build/CMakeFiles/SDL2pp.dir/SDL2pp/Point.cc.o"
+  "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/third_party/SDL2pp/SDL2pp/RWops.cc" "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/mapa/build/SDL2pp/build/CMakeFiles/SDL2pp.dir/SDL2pp/RWops.cc.o"
+  "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/third_party/SDL2pp/SDL2pp/Rect.cc" "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/mapa/build/SDL2pp/build/CMakeFiles/SDL2pp.dir/SDL2pp/Rect.cc.o"
+  "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/third_party/SDL2pp/SDL2pp/Renderer.cc" "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/mapa/build/SDL2pp/build/CMakeFiles/SDL2pp.dir/SDL2pp/Renderer.cc.o"
+  "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/third_party/SDL2pp/SDL2pp/SDL.cc" "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/mapa/build/SDL2pp/build/CMakeFiles/SDL2pp.dir/SDL2pp/SDL.cc.o"
+  "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/third_party/SDL2pp/SDL2pp/SDLImage.cc" "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/mapa/build/SDL2pp/build/CMakeFiles/SDL2pp.dir/SDL2pp/SDLImage.cc.o"
+  "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/third_party/SDL2pp/SDL2pp/SDLMixer.cc" "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/mapa/build/SDL2pp/build/CMakeFiles/SDL2pp.dir/SDL2pp/SDLMixer.cc.o"
+  "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/third_party/SDL2pp/SDL2pp/SDLTTF.cc" "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/mapa/build/SDL2pp/build/CMakeFiles/SDL2pp.dir/SDL2pp/SDLTTF.cc.o"
+  "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/third_party/SDL2pp/SDL2pp/Surface.cc" "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/mapa/build/SDL2pp/build/CMakeFiles/SDL2pp.dir/SDL2pp/Surface.cc.o"
+  "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/third_party/SDL2pp/SDL2pp/SurfaceLock.cc" "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/mapa/build/SDL2pp/build/CMakeFiles/SDL2pp.dir/SDL2pp/SurfaceLock.cc.o"
+  "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/third_party/SDL2pp/SDL2pp/Texture.cc" "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/mapa/build/SDL2pp/build/CMakeFiles/SDL2pp.dir/SDL2pp/Texture.cc.o"
+  "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/third_party/SDL2pp/SDL2pp/TextureLock.cc" "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/mapa/build/SDL2pp/build/CMakeFiles/SDL2pp.dir/SDL2pp/TextureLock.cc.o"
+  "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/third_party/SDL2pp/SDL2pp/Wav.cc" "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/mapa/build/SDL2pp/build/CMakeFiles/SDL2pp.dir/SDL2pp/Wav.cc.o"
+  "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/third_party/SDL2pp/SDL2pp/Window.cc" "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/mapa/build/SDL2pp/build/CMakeFiles/SDL2pp.dir/SDL2pp/Window.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "SDL2pp/build"
+  "/home/joaquin/Documents/Facultad/Taller/TP Final/Dune-Taller-de-Programacion-FIUBA/SDL/third_party/SDL2pp"
+  "/usr/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
